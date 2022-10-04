894a3edde4e602c379bbd0c7a894aadf520da7c1
wendy-fgt <chinwendu.onuoha@flutterwavego.com>
2022-10-04 15:22:29 +0100

    chore: npm initialize

---
+++ db/mysql.js
@@ -0,0 +1,13 @@
+const userPassword = process.env.PASSWORD;
+const userName = process.env.USERNAME
+const host = process.env.HOST
+const db = process.env.DATABASE
+
+const connection = mysql.createConnection({
+  host     : host,
+  user     : userName,
+  password :  userPassword,
+  database : db
+});
+
+module.exports = connection
---
+++ index.js
@@ -0,0 +1,2 @@
+const port = process.env.PORT 
+const express = require('express');
